a77e47febe2fa12d49fab1a639eda8a8aee629fd
James Brown <jbrown@easypost.com>
2022-04-01 20:35:19 +0000

    unpin a bunch of other dependencies

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ VERSION = '0.3.5'
 
 install_requires = [
     "arrow==0.17.0",
-    "cassandra-driver==3.24.0",
+    "cassandra-driver==3.*",
     "cassandra-migrate==0.3.5",
-    "click==7.1.2",
-    "future==0.18.2",
-    "geomet==0.2.1.post1",
-    "python-dateutil==2.8.1",
+    "click>=7.1.2,<9.0.0",
+    "future",
+    "geomet",
+    "python-dateutil==2.*",
     "PyYAML==5.*",
-    "six==1.15.0",
-    "tabulate==0.8.9",
-    "typing-extensions==3.7.4.3"]
+    "six==1.*",
+    "tabulate==0.8.*",
+    "typing-extensions==3.*"]
 
 setup(name='cassandra-migrate',
       packages=['cassandra_migrate'],
